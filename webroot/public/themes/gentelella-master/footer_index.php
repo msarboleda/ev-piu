8743a018c112ea4c339a986a07ad9dcaf662fa85
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-02-23 14:31:14 -0500

    Add the index template of EV PIU

---
+++ webroot/public/themes/gentelella-master/footer_index.php
@@ -0,0 +1,54 @@
+  <!-- Footer -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <ul class="list-inline text-center">
+            <li class="list-inline-item">
+              <a href="https://www.estradavelasquez.com/" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-globe fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/CI-Estrada-Vel%C3%A1squez-325003867570461/" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.youtube.com/channel/UC1bx3ozwACjoPOb_mVkec7Q" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-youtube fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.pinterest.es/ev_insumos/" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-pinterest fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+          <p class="copyright text-muted">Copyright &copy; CI Estrada Velasquez 1980 - <script>document.write(new Date().getFullYear());</script></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="<?php echo site_url('themes/clean-blog/vendor/jquery/jquery.min.js'); ?>"></script>
+  <script src="<?php echo site_url('themes/clean-blog/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="<?php echo site_url('themes/clean-blog/js/clean-blog.min.js'); ?>"></script>
+
+</body>
+</html>
