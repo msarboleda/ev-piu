ee1033f012e915e47ac3abef9d6ba100d66a226e
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-02-26 08:37:59 -0500

    Add the Dashboard template to EV PIU

---
+++ webroot/public/themes/gentelella-master/footer_evpiu.php
@@ -0,0 +1,19 @@
+      </div>
+      <!-- /page content -->
+
+      <!-- footer content -->
+      <footer>
+        <div class="pull-right">
+            Copyright &copy; CI Estrada Velasquez 1980 - <script>document.write(new Date().getFullYear());</script>
+        </div>
+        <div class="clearfix"></div>
+      </footer>
+      <!-- /footer content -->
+    </div>
+  </div>
+
+  <?php
+    echo Assets::js();
+  ?>
+</body>
+</html>
