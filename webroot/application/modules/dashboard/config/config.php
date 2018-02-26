3d1552842717f18c3711f04e555c5b1da48ce2cd
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-02-26 09:25:05 -0500

    Add the Dashboard module

---
+++ webroot/application/modules/dashboard/config/config.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+
+$config['module_config'] = array(
+  'name'        => 'Dashboard',
+  'description' => '',
+  'author'      => 'Estrada Velasquez Development',
+  'homepage'    => 'http://...',
+  'version'     => '1.0.1'
+);
